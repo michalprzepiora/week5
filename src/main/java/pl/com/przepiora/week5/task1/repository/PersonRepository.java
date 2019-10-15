0961f9240e249328d8a73f114d5f352a3ba0ab68
michalprzepiora <michal.przepiora@gmail.com>
2019-10-15 23:09:26 +0200

    Refactor Person class name done.

--- src/main/java/pl/com/przepiora/week5/task1/repository/PersonRepository.java
+++ src/main/java/pl/com/przepiora/week5/task1/repository/PersonRepository.java
@@ -1,23 +1,23 @@
 package pl.com.przepiora.week5.task1.repository;
 
 import org.springframework.web.client.RestTemplate;
-import pl.com.przepiora.week5.task1.model.PersonRepo;
-import pl.com.przepiora.week5.task1.model.Result;
+import pl.com.przepiora.week5.task1.model.Person;
+import pl.com.przepiora.week5.task1.model.PersonApiResponse;
 
 import java.util.List;
 
 public class PersonRepository {
 
   private static final String URL = "https://randomuser.me/api/?inc=picture,name,location,email,cell&results=10";
-  private List<Result> personList;
+  private List<Person> personList;
 
-  public List<Result> getPersonList() {
+  public List<Person> getPersonList() {
     return personList;
   }
 
   public PersonRepository() {
     RestTemplate restTemplate = new RestTemplate();
-    PersonRepo personRepo = restTemplate.getForObject(URL, PersonRepo.class);
-    personList = personRepo.getResults();
+    PersonApiResponse personApiResponse = restTemplate.getForObject(URL, PersonApiResponse.class);
+    personList = personApiResponse.getResults();
   }
 }
