85a7adbe4a059ef1e139034c813c0f8cad85d97a
michalprzepiora <michal.przepiora@gmail.com>
2019-10-21 22:33:35 +0200

    Add counter.

--- src/main/java/pl/com/przepiora/week5/vaadin/GameView.java
+++ src/main/java/pl/com/przepiora/week5/vaadin/GameView.java
@@ -14,88 +14,92 @@ import java.util.List;
 import java.util.Random;
 
 public class GameView extends VerticalLayout {
-    private List<RatesItem> rates;
-    private RatesItem selectedRate;
-    private double priceCurrency;
-    private TextField inputTextField;
-    private Label infoLabel;
-    private H1 points;
-    private int counter;
 
-    public GameView() {
-        VerticalLayout mainView = new VerticalLayout();
-        mainView.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        counter = 0;
+  private List<RatesItem> rates;
+  private RatesItem selectedRate;
+  private double priceCurrency;
+  private TextField inputTextField;
+  private Label infoLabel;
+  private H1 points;
+  private int counter;
 
+  public GameView() {
+    VerticalLayout mainView = new VerticalLayout();
+    mainView.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+    counter = 0;
 
-        CurrencyService currencyService = new CurrencyService();
-        rates = currencyService.getCurrency().getRates();
-        selectedRate = getRandomRate();
-        roundPriceCurrency();
+    CurrencyService currencyService = new CurrencyService();
+    rates = currencyService.getCurrency().getRates();
+    selectedRate = getRandomRate();
+    roundPriceCurrency();
 
-        Button drawRateButton = new Button("Losuj inną walutę");
-        mainView.add(drawRateButton);
+    Button drawRateButton = new Button("Losuj inną walutę");
+    mainView.add(drawRateButton);
 
-        H3 header = new H3(selectedRate.getCode() + " - " + selectedRate.getCurrency());
-        mainView.add(header);
+    H3 header = new H3(selectedRate.getCode() + " - " + selectedRate.getCurrency());
+    mainView.add(header);
 
-        inputTextField = new TextField("Wprowadź kurs");
-        mainView.add(inputTextField);
+    inputTextField = new TextField("Wprowadź kurs");
+    inputTextField.setWidth("100px");
+    mainView.add(inputTextField);
 
-        infoLabel = new Label("Wprowadź pierwszą wartość i nasiśnij enter");
-        mainView.add(infoLabel);
+    infoLabel = new Label("Wprowadź pierwszą wartość i nasiśnij enter");
+    mainView.add(infoLabel);
 
-        points = new H1(counter + " pkt.");
-        mainView.add(points);
+    points = new H1(counter + "  próba");
+    mainView.add(points);
 
-        add(mainView);
+    add(mainView);
 
-        drawRateButton.addClickListener(buttonClickEvent -> {
-            selectedRate = getRandomRate();
-            roundPriceCurrency();
-            header.setText(selectedRate.getCode() + " - " + selectedRate.getCurrency());
-            inputTextField.setEnabled(true);
-            infoLabel.setText("Wprowadź pierwszą wartość i nasiśnij enter");
-            counter = 0;
-            points.setText(counter + " pkt.");
-            inputTextField.setValue("");
-        });
+    drawRateButton.addClickListener(buttonClickEvent -> {
+      selectedRate = getRandomRate();
+      roundPriceCurrency();
+      header.setText(selectedRate.getCode() + " - " + selectedRate.getCurrency());
+      inputTextField.setEnabled(true);
+      infoLabel.setText("Wprowadź pierwszą wartość i nasiśnij enter");
+      counter = 0;
+      points.setText(counter + "  próba");
+      inputTextField.setValue("");
+    });
 
-        inputTextField.addValueChangeListener(event -> {
-          if (event.getValue().equals("")) return;
-            try {
-                double price = Double.parseDouble(event.getValue());
-                if (price > priceCurrency) {
-                    infoLabel.setText("Za dużo");
-                    increasePoints();
-                } else if (price < priceCurrency) {
-                    infoLabel.setText("Za mało");
-                    increasePoints();
-                } else {
-                    infoLabel.setText("Gratki! Udało się za " + counter + " razem!");
-                    inputTextField.setEnabled(false);
-                }
+    inputTextField.addValueChangeListener(event -> {
+      if (event.getValue().equals("")) {
+        return;
+      }
+      try {
+        String value = event.getValue().replace(",", ".");
+        double price = Double.parseDouble(value);
+        if (price > priceCurrency) {
+          infoLabel.setText("Za dużo");
+          increasePoints();
+        } else if (price < priceCurrency) {
+          infoLabel.setText("Za mało");
+          increasePoints();
+        } else {
+          infoLabel.setText("Gratki! Udało się za " + counter + " razem!");
+          inputTextField.setEnabled(false);
+        }
 
-            } catch (Exception e) {
-                Notification.show("Wprowadzono nieprawidłową wartość.", 3000, Notification.Position.MIDDLE);
-            }
-        });
+      } catch (Exception e) {
+        Notification.show("Wprowadzono nieprawidłową wartość.", 3000, Notification.Position.MIDDLE);
+      }
+    });
 
-    }
+  }
 
-    private void increasePoints() {
-        counter++;
-        points.setText(counter + "pkt.");
-    }
+  private void increasePoints() {
+    counter++;
+    points.setText(counter + " próba");
+  }
 
-    private void roundPriceCurrency() {
-        priceCurrency = Math.round(selectedRate.getMid() * 100);
-        priceCurrency /= 100;
-    }
+  private void roundPriceCurrency() {
+    priceCurrency = Math.round(selectedRate.getMid() * 100);
+    priceCurrency /= 100;
+  }
 
-    private RatesItem getRandomRate() {
-        Random random = new Random();
-        int index = random.nextInt(rates.size());
-        return rates.get(index);
-    }
+  private RatesItem getRandomRate() {
+    Random random = new Random();
+    int index = random.nextInt(rates.size());
+    return rates.get(index);
+  }
 }
--- src/main/java/pl/com/przepiora/week5/vaadin/MainView.java
+++ src/main/java/pl/com/przepiora/week5/vaadin/MainView.java
@@ -8,20 +8,32 @@ import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Route
 @UIScope
 public class MainView extends VerticalLayout {
+
   static List<String> counter = new ArrayList<>();
 
   private VerticalLayout mainLayout;
 
   public MainView() {
-    counter.add(counter.size() + "  ------ " + LocalDateTime.now().toString());
+    InetAddress addr;
+    try {
+      addr = InetAddress.getLocalHost();
+      String hostname = addr.getHostName();
+      counter.add(counter.size() + "     date: " + LocalDate.now() + "    time: " + LocalTime.now()
+          .getHour() + ":" + LocalTime.now().getMinute() + "  " + hostname);
+    } catch (UnknownHostException e) {
+      System.err.println("Unknown host: " + e.getMessage());
+    }
+
     this.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
     mainLayout = new VerticalLayout();
     mainLayout.setWidth("60%");
@@ -38,6 +50,8 @@ public class MainView extends VerticalLayout {
     Tab task2tab = new Tab("2. Pogodynka");
     Tab task3tab = new Tab("3. Gra walutowa");
     Tabs menuTabs = new Tabs(task1tab, task2tab, task3tab);
+    menuTabs.setSelectedTab(task1tab);
+    mainLayout.add(task1View);
     menuTabs.addSelectedChangeListener(event -> {
       mainLayout.removeAll();
       if (event.getSelectedTab().equals(task1tab)) {
